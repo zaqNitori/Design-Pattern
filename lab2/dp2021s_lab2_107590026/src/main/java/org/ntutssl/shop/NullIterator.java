ea2ad21b6aa22b7ffccbf5bb45a447a60933fdb6
zaqNatsuki <zaq155162@yahoo.com>
2021-06-01 13:20:15 +0800

    UPload: lab2

---
+++ lab2/dp2021s_lab2_107590026/src/main/java/org/ntutssl/shop/NullIterator.java
@@ -0,0 +1,17 @@
+package org.ntutssl.shop;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class NullIterator implements Iterator<Goods> 
+{
+	public Goods next() 
+	{ 
+	throw new NoSuchElementException("No Such Element!");
+	}
+
+	public boolean hasNext() 
+	{ 
+	return false;
+	}
+}
